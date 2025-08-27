ce399c6bcb24685992956021b9698b1328a0409a
CyberMan010 <mohamahasoun60@gmail.com>
2025-08-27 17:20:59 +0300

    feat: graphql added successfully

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      { "id": "P-1001", "name": "12mm Hex Bolt", "sku": "HEX-12-100", "warehouse": "BLR-A", "stock": 180, "demand": 120 },
+  { "id": "P-1002", "name": "Steel Washer", "sku": "WSR-08-500", "warehouse": "BLR-A", "stock": 50, "demand": 80 },
+  { "id": "P-1003", "name": "M8 Nut", "sku": "NUT-08-200", "warehouse": "PNQ-C", "stock": 80, "demand": 80 },
+  { "id": "P-1004", "name": "Bearing 608ZZ", "sku": "BRG-608-50", "warehouse": "DEL-B", "stock": 24, "demand": 120 }
+
+    `,
+  })
+  .then((result) => console.log(result));
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+   <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
+   </ApolloProvider>,
 )
